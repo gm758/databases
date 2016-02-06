74166db2003fe193a834304b422b324b67684126
Griffin Michl <griffinmichl@gmail.com>
2016-02-05 16:29:40 -0800

    fix server-spec tests

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,24 @@
 var db = require('../db');
 
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  'Content-Type': "application/json"
+};
+
+
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (response, data, statusCode) {
+      statusCode = statusCode || 200;
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(data));
+    }, // a function which produces all the messages
+    post: function () {
+      
+    } // a function which can be used to insert a message into the database
   },
 
   users: {

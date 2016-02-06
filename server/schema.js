2742bf6df20d055a51621761c9f704216b399403
Griffin Michl <griffinmichl@gmail.com>
2016-02-06 12:36:35 -0800

    export sequelize machinery from schema

--- server/schema.js
+++ server/schema.js
@@ -1,28 +1,29 @@
 var config = require('./serverConfig.js');
 
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('chat', 'root', config.MYSQL_PASSWORD);
-var User = sequelize.define('user', {
+exports.sequelize = new Sequelize('chat', 'root', config.MYSQL_PASSWORD);
+
+exports.User = exports.sequelize.define('user', {
   id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
   username: {type: Sequelize.STRING, unique: true}
 });
 
-var Room = sequelize.define('room', {
+exports.Room = exports.sequelize.define('room', {
   id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
   roomname: {type: Sequelize.STRING, unique: true}
 });
 
-var Message = sequelize.define('message', {
+exports.Message = exports.sequelize.define('message', {
   id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
   content: Sequelize.STRING,
-  user_id: {
+  'user_id': {
     type: Sequelize.INTEGER,
     referenecs: {
       model: User,
       key: 'id'
     }
   },
-  room_id: {
+  'room_id': {
     type: Sequelize.INTEGER,
     referenecs: {
       model: Room,
@@ -31,16 +32,14 @@ var Message = sequelize.define('message', {
   }
 });
 
-User.sync({force: true}).then(function() {
-  console.log('test');
-
-  User.create({username: 'Chatterbox'});
+exports.User.sync({force: true}).then(function() {
+  exports.User.create({username: 'Chatterbox'});
 }).then(function() {
-  Room.sync({force: true}).then(function() {
-    Room.create({roomname: 'lobby'});
+  exports.Room.sync({force: true}).then(function() {
+    exports.Room.create({roomname: 'lobby'});
   }).then(function(){
-    Message.sync({force: true}).then(function() {
-      Message.create({content: 'Welcome!', user_id: 1, room_id: 1});
+    exports.Message.sync({force: true}).then(function() {
+      exports.Message.create({content: 'Welcome!', 'user_id': 1, 'room_id': 1});
     });
   });
 });

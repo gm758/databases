e21bb0b042a7ef1f05bc0cb09c7a5fee4daf164b
Griffin Michl <griffinmichl@gmail.com>
2016-02-05 20:22:27 -0800

    serve jquery

--- server/app.js
+++ server/app.js
@@ -23,7 +23,8 @@ app.use(parser.json());
 app.use('/classes', router);
 
 // Serve the client files
-
+console.log('test');
+app.use(express.static(path.join(__dirname + '/../bower_components/jquery/dist')));
 app.use(express.static(__dirname + '/../client'));
 
 // If we are being run directly, run the server.
